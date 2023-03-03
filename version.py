e380ed9156cdc27f89ae261a9f53d2b2fee73294
Anyelorp <1486492@senati.pe>
2023-03-03 10:03:23 -0500

    desarrollo

---
+++ version.py
@@ -0,0 +1,13 @@
+ # Desarrolle un algoritmo para hallar el area de un 
+ # circulo, el usuario debe ingresar el radio
+ 
+#include<stdio.h>
+import math
+
+# mostrar el radio
+print("coloque el radio del circulo")
+# capturar el radio
+radio=float(input())
+# obtener el valor de pi
+pi=math.pi
+print("vaolr de PI ",pi)
